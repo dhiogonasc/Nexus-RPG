555a8b66c689e86407cd86a86eeb533eacc2e8e7
dhiogonasc <dhiogonasc.es@gmail.com>
2026-03-09 14:34:22 -0400

    implementando doc - user request/response

--- api/src/main/java/com/nexus/nexusrpg/controller/dto/response/UsuarioResponseDTO.java
+++ api/src/main/java/com/nexus/nexusrpg/controller/dto/response/UsuarioResponseDTO.java
@@ -1,10 +1,20 @@
 package com.nexus.nexusrpg.controller.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDateTime;
 
 public record UsuarioResponseDTO(
+
+        @Schema(description = "Identificação do usuário")
         Long id,
+
+        @Schema(description = "Nome de usuário")
         String nome,
+
+        @Schema(description = "Endereço de email do usuário")
         String email,
+
+        @Schema(description = "Data e hora de cadastro do usuário")
         LocalDateTime dataCriacao
 ) {}
