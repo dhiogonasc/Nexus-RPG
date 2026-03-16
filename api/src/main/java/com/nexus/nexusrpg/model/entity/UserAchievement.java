df8945aa68df7fa7848073af4f20973852e1f102
dhiogonasc <dhiogonasc.es@gmail.com>
2026-03-16 10:47:13 -0400

    UserAchievement.java

---
+++ api/src/main/java/com/nexus/nexusrpg/model/entity/UserAchievement.java
@@ -0,0 +1,37 @@
+package com.nexus.nexusrpg.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(
+        name = "user_achievement",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uk_user_achievement_pair", columnNames = {"user_id", "achievement_id"})
+        }
+)
+public class UserAchievement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "achievement_id", nullable = false)
+    private Achievement achievement;
+
+    @CreationTimestamp
+    @Column(name = "collected_at", nullable = false, updatable = false)
+    private LocalDateTime collectedAt;
+}
