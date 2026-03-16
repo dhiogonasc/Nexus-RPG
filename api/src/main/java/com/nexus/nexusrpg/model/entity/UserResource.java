92b31cb55249c24eaaefb17e1d8acb75844b2b91
dhiogonasc <dhiogonasc.es@gmail.com>
2026-03-16 10:32:31 -0400

    UserResource.java

--- api/src/main/java/com/nexus/nexusrpg/model/entity/UserResource.java
+++ api/src/main/java/com/nexus/nexusrpg/model/entity/UserResource.java
@@ -1,30 +1,34 @@
 package com.nexus.nexusrpg.model.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "user_resource")
+@Table(name = "\"user_resource\"", uniqueConstraints = {
+        @UniqueConstraint(name = "uk_user_resource", columnNames = {"user_id", "resource_id"})
+})
 public class UserResource {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user_id", nullable = false)
-    private Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "resource_id", nullable = false)
     private Resource resource;
 
-    @Column(nullable = false)
-    private Integer quantity;
+    @CreationTimestamp
+    @Column(name = "\"collected_at\"", nullable = false, updatable = false)
+    private LocalDateTime collectedAt;
 }
