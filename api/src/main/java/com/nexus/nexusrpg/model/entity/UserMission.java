3ed401484330eed8f2874f367563d62ecd9b024e
dhiogonasc <dhiogonasc.es@gmail.com>
2026-03-16 10:08:46 -0400

    UserMission.java

--- api/src/main/java/com/nexus/nexusrpg/model/entity/UserMission.java
+++ api/src/main/java/com/nexus/nexusrpg/model/entity/UserMission.java
@@ -11,27 +11,33 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "user_mission")
+@Table(name = "\"user_mission\"")
 public class UserMission {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "mission_id", nullable = false)
     private Mission mission;
 
     @Builder.Default
     @Enumerated(EnumType.STRING)
-    @Column(name = "status", nullable = false, columnDefinition = "mission_status")
+    @Column(name = "\"status\"", nullable = false, columnDefinition = "mission_status")
     private MissionStatus status = MissionStatus.IN_PROGRESS;
 
     @Builder.Default
-    @Column(name = "best_result", nullable = false)
-    private BigDecimal bestResult = new BigDecimal(0);
+    @Column(
+            name = "\"best_result\"",
+            nullable = false,
+            precision = 5,
+            scale = 2,
+            columnDefinition = "score DEFAULT 0"
+    )
+    private BigDecimal bestResult = BigDecimal.ZERO;
 }
