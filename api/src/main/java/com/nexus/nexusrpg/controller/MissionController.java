4118f2d93747f577b01f1484792a51a06f777620
dhiogonasc <dhiogonasc.es@gmail.com>
2026-03-19 17:01:23 -0400

    importando dependencias de MissionService

--- api/src/main/java/com/nexus/nexusrpg/controller/MissionController.java
+++ api/src/main/java/com/nexus/nexusrpg/controller/MissionController.java
@@ -1,15 +1,25 @@
+package com.nexus.nexusrpg.controller;
+
+import com.nexus.nexusrpg.controller.dto.mission.MissionDTO;
+import com.nexus.nexusrpg.mapper.MissionMapper;
+import com.nexus.nexusrpg.repository.MissionRepository;
+import lombok.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/mission")
 @RequiredArgsConstructor
 public class MissionController {
 
-    private final MissionService missionService;
+    private final MissionRepository missionRepository;
     private final MissionMapper missionMapper;
-
 
     @GetMapping
     public ResponseEntity<List<MissionDTO>> listarTodos() {
-        List<MissionDTO> missions = missionService.findAll()
+        List<MissionDTO> missions = missionRepository.findAll()
                 .stream()
                 .map(missionMapper::toDTO)
                 .toList();
@@ -19,7 +29,7 @@ public class MissionController {
     @GetMapping("/{id}")
     public ResponseEntity<MissionDTO> buscarPorId(@PathVariable Long id) {
         return ResponseEntity.ok(
-                missionMapper.toDTO(missionService.findById(id))
+                missionMapper.toDTO(missionRepository.findById(id))
         );
     }
 }
