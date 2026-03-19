26a4ea63a2b9e8d3130e403d22c1f474991f0ca2
dhiogonasc <dhiogonasc.es@gmail.com>
2026-03-19 16:45:36 -0400

    importando dependencias em PlanetController

--- api/src/main/java/com/nexus/nexusrpg/controller/PlanetController.java
+++ api/src/main/java/com/nexus/nexusrpg/controller/PlanetController.java
@@ -1,15 +1,27 @@
+package com.nexus.nexusrpg.controller;
+
+import com.nexus.nexusrpg.controller.dto.planet.PlanetDTO;
+import com.nexus.nexusrpg.mapper.PlanetMapper;
+import com.nexus.nexusrpg.repository.PlanetRepository;
+import lombok.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/planet")
 @RequiredArgsConstructor
 public class PlanetController {
 
-    private final PlanetService planetService;
+    private final PlanetRepository planetRepository;
     private final PlanetMapper planetMapper;
 
 
     @GetMapping
     public ResponseEntity<List<PlanetDTO>> listarTodos() {
-        List<PlanetDTO> planets = planetService.findAll()
+
+        List<PlanetDTO> planets = planetRepository.findAll()
                 .stream()
                 .map(planetMapper::toDTO)
                 .toList();
@@ -20,8 +32,7 @@ public class PlanetController {
 
     @GetMapping("/{id}")
     public ResponseEntity<PlanetDTO> buscarPorId(@PathVariable Long id) {
-        return ResponseEntity.ok(
-                planetMapper.toDTO(planetService.findById(id))
-        );
+
+        return ResponseEntity.ok(planetMapper.toDTO(planetRepository.findById(id).orElseThrow()));
     }
 }
