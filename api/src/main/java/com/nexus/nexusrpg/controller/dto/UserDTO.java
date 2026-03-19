d4f42ce4203bf47c490e0ef9c9f415f6f2f5d005
dhiogonasc <dhiogonasc.es@gmail.com>
2026-03-19 13:34:13 -0400

    UserDTO.java

--- api/src/main/java/com/nexus/nexusrpg/controller/dto/UserDTO.java
+++ api/src/main/java/com/nexus/nexusrpg/controller/dto/UserDTO.java
@@ -3,6 +3,8 @@ package com.nexus.nexusrpg.controller.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+import java.util.List;
+
 @Tag(name = "User DTO")
 public record UserDTO(
 
@@ -18,8 +20,20 @@ public record UserDTO(
         @Schema(description = "Nível atual")
         LevelDTO level,
 
+        @Schema(description = "Planeta onde o usuário se encontra no momento")
+        PlanetDTO currentPlanet,
+
+        @Schema(description = "Lista de nomes dos planetas já visitados/desbloqueados")
+        List<PlanetDTO> unlockedPlanets,
+
+        @Schema(description = "Planeta onde o usuário se encontra no momento")
+        MissionDTO currentMission,
+
+        @Schema(description = "Lista de nomes dos planetas já visitados/desbloqueados")
+        List<MissionDTO> unlockedMissions,
+
         @Schema(description = "Xp total do usuário")
-        int xp,
+        long xp,
 
         @Schema(description = "Quantidade de oxigênio disponível")
         int oxygen
