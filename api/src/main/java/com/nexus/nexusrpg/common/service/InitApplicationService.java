9baaa5f02d6c1ba5482c07dc277d50e5d817b378
dhiogonasc <dhiogonasc.es@gmail.com>
2026-04-14 20:56:01 -0400

    initializers

--- api/src/main/java/com/nexus/nexusrpg/common/service/InitApplicationService.java
+++ api/src/main/java/com/nexus/nexusrpg/common/service/InitApplicationService.java
@@ -1,39 +1,26 @@
 package com.nexus.nexusrpg.common.service;
 
+import com.nexus.nexusrpg.common.interfaces.Initializable;
 import com.nexus.nexusrpg.domain.level.model.Level;
 import com.nexus.nexusrpg.domain.level.service.LevelService;
-import com.nexus.nexusrpg.domain.mission.service.InitMissionService;
-import com.nexus.nexusrpg.domain.planet.service.InitPlanetService;
-import com.nexus.nexusrpg.domain.resource.service.InitResourceService;
 import com.nexus.nexusrpg.domain.user.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class InitApplicationService {
 
     private final LevelService levelService;
-
-    private final InitPlanetService initPlanetService;
-    private final InitMissionService initMissionService;
-    private final InitResourceService initResourceService;
+    private final List<Initializable<?>> initializers;
 
     public void initUser(User user) {
-
-        var initialPlanets = initPlanetService.initialize(user);
-        var initialMissions = initMissionService.initialize(user);
-        var initialResources = initResourceService.initialize(user);
-
-        user.initialize(
-                initialPlanets,
-                initialMissions,
-                initialResources
-        );
+        initializers.forEach(initializer -> initializer.initialize(user));
     }
 
     public Level initialLevel() {
-
         return levelService.initialLevel();
     }
 }
